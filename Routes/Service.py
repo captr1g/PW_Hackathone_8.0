c81477b299d7f34f3b722766c26cc8c76b1fe93d
Yash Raj Saxena <yashrajsaxena1@gmail.com>
2023-04-30 10:45:47 +0530

    jinja templets and responce added to the Routes

--- Routes/Service.py
+++ Routes/Service.py
@@ -1,9 +1,13 @@
 from typing import List
 from fastapi import APIRouter, Depends, status, Request
 from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from Backend import Schema, ServiceServer, AuthorizeServer as Auth
 
 router = APIRouter(prefix='/services', tags=['Services'])
+template = Jinja2Templates(directory="Frontend")
+
 
 @router.get("/", status_code=status.HTTP_302_FOUND)
 def service_home(request:Request, current_user:Schema.UserData=Depends(Auth.get_current_user)):
@@ -18,13 +22,13 @@ def add_money(request:Schema.AddMoney, current_user:Schema.UserData=Depends(Auth
 def send_money(request:Schema.GroupNewTransaction, current_user:Schema.UserData=Depends(Auth.get_current_user)):
     money = ServiceServer.Send_Money(request, current_user.username)
     return money
-
-@router.get("/remainder", status_code=status.HTTP_200_OK)
+@router.get("/remainder", status_code=status.HTTP_200_OK, response_class=HTMLResponse)
 def get_remainder(request:Request, current_user:Schema.UserData=Depends(Auth.get_current_user)):
     remainder = ServiceServer.Get_Remainders(current_user.username)
-    return remainder
+    return template.TemplateResponse("remaninder.html",{"request":request, "remaninder": remainder})
 
-@router.post("/remainder", status_code=status.HTTP_202_ACCEPTED)
+
+@router.post("/remainder", status_code=status.HTTP_202_ACCEPTED, response_class=HTMLResponse)
 def pay_debt(request:Schema.PayDebt, current_user:Schema.UserData=Depends(Auth.get_current_user)):
     payment = ServiceServer.Pay_Debt(request, current_user.username)
     return payment
